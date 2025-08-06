ec7465bf3b076855d082a0194803376f4b62cc17
Your Name <you@example.com>
2025-08-06 08:31:52 +0300

    basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("test");
+use xitca_web::{handler::handler_service, route::get, App};
+
+fn main() -> std::io::Result<()> {
+    App::new()
+        .at("/", get(handler_service(async || "Hello, World!")))
+        .serve()
+        .bind("127.0.0.1:8080")?
+        .run()
+        .wait()
 }
